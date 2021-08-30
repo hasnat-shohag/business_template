bdb952bd92c87d68e45ce2319a6a9d4a6ad49193
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2021-08-30 14:17:52 +0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".button");
+
+button.addEventListener('click',function(e){
+    e.preventDefault();
+    // document.getElementsByClassName('nav_item').show('active');
+})
