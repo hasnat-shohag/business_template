5509b2e9654fe6c0f541bd0f708a405594d2b89a
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2021-09-01 00:18:35 +0600

    plugin added, overfollow control

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@ button.addEventListener('click',function(e){
     e.preventDefault();
     // document.getElementsByClassName('nav_item').show('active');
 })
+
+
+
+$(function () {
+    $('.team_slide').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: false,
+        animateOut: 'rotateIn',
+        animateIn: 'rotateInDownRight',
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            992: {
+                items: 2
+            }
+        }
+    });
+})
